dafe227ec527c435697e397f9b978bf140efc6a1
Jonah Walker <supposedlysam@gmail.com>
2022-10-15 18:51:46 -0600

    feat: add chunk to state object

--- examples_ext/flutter_infinite_list/lib/posts/bloc/post_state.dart
+++ examples_ext/flutter_infinite_list/lib/posts/bloc/post_state.dart
@@ -4,21 +4,25 @@ enum PostStatus { initial, success, failure }
 
 class PostState extends Equatable {
   const PostState({
+    this.chunk = const Chunk(limit: 20),
     this.status = PostStatus.initial,
     this.posts = const <Post>[],
     this.hasReachedMax = false,
   });
 
+  final Chunk<Post, int> chunk;
   final PostStatus status;
   final List<Post> posts;
   final bool hasReachedMax;
 
   PostState copyWith({
+    Chunk<Post, int>? chunk,
     PostStatus? status,
     List<Post>? posts,
     bool? hasReachedMax,
   }) {
     return PostState(
+      chunk: chunk ?? this.chunk,
       status: status ?? this.status,
       posts: posts ?? this.posts,
       hasReachedMax: hasReachedMax ?? this.hasReachedMax,
@@ -27,9 +31,11 @@ class PostState extends Equatable {
 
   @override
   String toString() {
-    return '''PostState { status: $status, hasReachedMax: $hasReachedMax, posts: ${posts.length} }''';
+    final readableChunk =
+        '''Chunk(dataLength: ${chunk.data.length}, cursor: ${chunk.cursor}, limit: ${chunk.limit})''';
+    return '''PostState { chunk: $readableChunk, status: $status, hasReachedMax: $hasReachedMax, posts: ${posts.length} }''';
   }
 
   @override
-  List<Object> get props => [status, posts, hasReachedMax];
+  List<Object> get props => [chunk, status, posts, hasReachedMax];
 }
